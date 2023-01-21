888d0db514e8412a10e9b5b4f89d1739e9578680
Ojidaemo <vit.martsinovich@gmail.com>
2023-01-21 18:30:15 +0300

    Initial Commit

---
+++ MagicBall_code/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MagicBall_code
+//
+//  Created by Vitali Martsinovich on 2023-01-21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
